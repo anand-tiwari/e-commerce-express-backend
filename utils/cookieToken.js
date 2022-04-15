1c7cc00b0b63168279536eb039ac73fd9a4bffdb
anand.tiwari <anand.tiwari@razorpay.com>
2022-04-16 03:30:00 +0530

    added changes related to env

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -3,8 +3,8 @@ const cookieToken = (user) => {
   const options = {
     expires: new Date(Date.now() + 3 * 24 * 60 * 1000),
     httpOnly: true,
-    /*secure:true,
-    samesite:"none"*/
+    secure:true,
+    samesite:"none"
   };
   return { token, options };
 };
